f98d9c345128545c1af5a32bd2e8473c92e765a6
Cybergavin <cybergavin@gmail.com>
2022-04-21 13:32:37 -0400

    Modified to use environment variables for endpoint and clientid

--- app-configuration/appcs-umi.js
+++ app-configuration/appcs-umi.js
@@ -1,21 +1,30 @@
 // App Configuration demo - https://docs.microsoft.com/en-us/azure/azure-app-configuration/quickstart-javascript?tabs=azure-powershell
 // cybergavin
-// Retrieve configuration (key-value) and feature flag from App Configuration using User-assigned managed identity
-// Pre-requisite: npm install @azure/app-configuration @azure/identity
-// 
+// Retrieve configuration (key-value) and feature flag from App Configuration using a user-assigned managed identity
+// Pre-requisite: npm install @azure/app-configuration @azure/identity dotenv
+//////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+// Source environment variables 
+// APP_CONFIGURATION_ENDPOINT = App configuration endpoint URL (https://xxx.azconfig.io)
+// IDENTITY_CLIENTID = Managed Identity client_id
+require('dotenv').config();
+
+// Include Azure modules
 const appConfig = require("@azure/app-configuration");
 const appId = require("@azure/identity");
-// Use user-assigned managed identity for credential
+
+// Create credential for user-assigned managed identity
 const credential = new appId.DefaultAzureCredential({
-    managedIdentityClientId: "50m3n0n53n537f@#%^%^^^^-48a4408a57c4"
+    managedIdentityClientId: String(process.env.IDENTITY_CLIENTID) 
   });
-// connect using connection string with read-only access key
-const connection_string = "https://moneris365dev-conf-gstest.azconfig.io";
-const client = new appConfig.AppConfigurationClient(connection_string, credential);
+  
+// Connect to App configuration
+const client = new appConfig.AppConfigurationClient(process.env.APP_CONFIGURATION_ENDPOINT, credential);
+
+// Retrieve configuration
 async function run() {
-  // retrieve key "demo1:url:backend" 
+  // retrieve key-value "demo1host" 
   let retrievedSetting = await client.getConfigurationSetting({
-    key: "demo1:url:backend"
+    key: "demo1host"
   });
   // retrieve feature flag "debug" (JSON value returned)
   let dflag = await client.getConfigurationSetting({
